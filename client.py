86bbc91a8aa0a86e40fcd0a9d1c2db713ba5e72d
Administrator <Administrator@VAV3Z7EUM7XFZUA>
2018-09-13 17:34:09 +0800

    初始化项目

---
+++ client.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+
+class Client:
+    @staticmethod
+    def test(url):
+        browser = webdriver.Chrome360()
+        browser.get('http://www.baidu.com')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
